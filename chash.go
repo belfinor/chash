ae2f3627f76a5884c5e741d39e7239e9098a160d
Mikhail Kirillov <mikkirillov@yandex.ru>
2019-07-12 16:48:34 +0300

    chash base

---
+++ chash.go
@@ -0,0 +1,37 @@
+package chash
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2017-11-01
+
+import (
+	"hash/crc32"
+)
+
+type Hash struct {
+	num  int
+	step uint32
+}
+
+func New(nodes int) *Hash {
+	h := &Hash{
+		num:  nodes,
+		step: uint32(0xffffffff) / uint32(nodes),
+	}
+
+	return h
+}
+
+func (h *Hash) Get(key []byte) int {
+	v := crc32.ChecksumIEEE(key)
+	num := int(v / h.step)
+	if num >= h.num {
+		num = h.num - 1
+	}
+
+	return num
+}
+
+func (h *Hash) Next(num int) int {
+	return (num + 1) % h.num
+}
---
+++ chash_test.go
@@ -0,0 +1,23 @@
+package chash
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2018-07-04
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestConsistent(t *testing.T) {
+	hash := New(10)
+
+	wait := []int{9, 5, 1, 4, 9, 5, 1, 4, 9, 5}
+
+	for i := int64(0); i < 10; i++ {
+		key := []byte(strconv.FormatInt(i, 10))
+		if hash.Get(key) != wait[int(i)] {
+			t.Fatal("invalid function value")
+		}
+	}
+}
